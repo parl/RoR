eb2c9c21d91220e65cdeb24f629f5e07914b1330
parl <nopalnurhamid@gmail.com>
2023-11-01 11:16:47 +0700

    add API

---
+++ app/controllers/api/books_controller.rb
@@ -0,0 +1,7 @@
+class Api::BooksController < ApplicationController
+  def index
+    @books = Book.all
+
+    render json: @books
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'books/index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
   registrations: 'users/registrations'
